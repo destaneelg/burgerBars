6c2a745a8d33d1e79ad369479410b0e64ebfd56f
Destanee Glenn <destanee.glenn@gmail.com>
2020-10-22 21:30:30 -0500

    addingcode

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burgernator', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mig Bac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shopper', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('HipModdy', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Trouble Troublr', true);
