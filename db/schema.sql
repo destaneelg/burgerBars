edf05bc9f1f34cd9b17078edcb95d55dcca3dc32
Destanee Glenn <destanee.glenn@gmail.com>
2020-12-21 12:41:48 -0600

    recloning and fixing routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,11 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
+DROP TABLE IF EXISTS burgers; 
 
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(30) NOT NULL,
+		name varchar(30) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Burgernator', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mig Bac', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Shopper', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('HipModdy', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Trouble Troublr', true);
+
+INSERT INTO burgers (name, devoured) VALUES ('Shopper', false);
+INSERT INTO burgers (name, devoured) VALUES ('HipModdy', false);
+INSERT INTO burgers (name, devoured) VALUES ('Trouble Trouble', false);
