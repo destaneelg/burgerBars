1ad46fcfa1f577eed4259cf776676baa37e96013
Destanee Glenn <destanee.glenn@gmail.com>
2020-12-25 09:34:11 -0600

    deploying

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,7 @@
 var mysql = require("mysql");
 
-var connection; 
-
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-  connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "purpleR0s",
-      database: "burgers_db"
-});
-};
-connection.connect(function(err) {
-  if (err) {
-    console.error("could not connect : " + err.stack);
-    return;
-  }
-  console.log("You are now connected " + connection.threadId);
-});
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 
+connection.connect();
 module.exports = connection;
